db87e38468f29b7679312c1760ff8c8cd37d625b
andrewblance <andrewblance@live.co.uk>
2018-01-02 22:54:00 +0000

    Now functions correctly

--- dxdt.cpp
+++ dxdt.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 // Choose Initial Values
-double x_init = 1., y_init = 0., Vx_init = 0., Vy_init = 1. , theta_init = 0., w_init = 0.; 
+double x_init = 1., y_init = 0., Vx_init = 0., Vy_init = 2*M_PI , theta_init = 0., w_init = 0.; 
 double x0[] = { x_init, y_init, Vx_init, Vy_init, theta_init, w_init };
-double t0 = 0., t1 = 45., dt = 0.0001;
+double t0 = 0., t1 = 9., dt = 0.0001;
 
 const size_t n_dim = 6;
 Vector x( x0, n_dim );
@@ -44,10 +44,10 @@ Vector dx_dt( const double t, const Vector x)
 
     output[0] = x[2];   
     output[1] = x[3];
-    output[2] = - kappa*x[0] / pow(r, exponent+1.);
-    output[3] = - kappa*x[1] / pow(r, exponent+1.);
+    output[2] = -GMs* kappa*x[0] / pow(r, exponent+1.);
+    output[3] = -GMs* kappa*x[1] / pow(r, exponent+1.);
     output[4] = x[5];
-    output[5] = - (3./pow(r,5)) * (x[0]*sin(x[4]) - x[1]*cos(x[4])) * (x[0]*cos(x[4]) + x[1]*sin(x[4]));
+    output[5] = - (3.*GMs/pow(r,5)) * (x[0]*sin(x[4]) - x[1]*cos(x[4])) * (x[0]*cos(x[4]) + x[1]*sin(x[4]));
     
     return output;
 }
