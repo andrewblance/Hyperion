2fab154b4fde0237097831304f1a303a78d14660
Andrew Blance <andrewblance@live.co.uk>
2018-01-30 16:12:35 +0000

    For theta/time

--- plotter.py
+++ plotter.py
@@ -6,32 +6,16 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt('results.csv', delimiter = ',', dtype =None)
 columns = len(data[1,:])
 
-title_input = input("What is the title of the plot? ")
+xdata = data[:,0]
+ydata = data[:,5]
 
-print("Lets choose the axis for the plot!")
-if columns == 3:
-    print("The options are: ")
-    print("0: time, 1: x, 2: Vx")
-if columns == 5: 
-    print("The options are: ")
-    print("0: time, 1: x, 2: Vx, 3: y, 4: Vy")
-if columns == 7:
-    print("The options are: ")
-    print("0: time, 1: x, 2: Vx, 3: y, 4: Vy, 5: z, 6: Vz")
+plt.rc('text', usetex=True)
+plt.plot(xdata, ydata)
+plt.title(r'Hyperion $\theta$ versus time ($\theta_0 = 0$)', fontsize=20)
+plt.text(4.5,3.6,'Circular Orbit', horizontalalignment='center', fontsize=18)
 
-xinput = input("What data do you want shown on the x axis? ")
-x_label = input("What is the label for the x axis? ")
-xdata = data[:,xinput]
-
-yinput = input("What data do you want shown on the y axis? ")
-y_label = input("What is the label for the y axis? ")
-ydata = data[:,yinput]
-
-plt.scatter(xdata, ydata, s=0.1)
-plt.grid(True)
-plt.title(title_input)
-plt.xlabel(x_label)
-plt.ylabel(y_label)
+plt.xlabel('time (yr)', fontsize=18)
+plt.ylabel(r'$\theta$ (radians)', fontsize=18)
 
 plt.savefig("Results.png")
 plt.show()
